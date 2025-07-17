b3a7623713e40244016ceadba4d3be4f32e412c6
Joao Vicente <joaogvicente@gmail.com>
2025-07-17 23:23:23 +0100

    added bridge aux functions

--- pkg/network/backend/bridge.go
+++ pkg/network/backend/bridge.go
@@ -3,6 +3,9 @@ package backend
 import (
 
 	// std
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
 	"net"
 	"net/netip"
 	"syscall"
@@ -10,7 +13,21 @@ import (
 	"github.com/vishvananda/netlink"
 )
 
-func CreateBridge(bridgeName string, mtu int, gateway netip.Addr) (netlink.Link, error) {
+const (
+	brPrefix    = "br-"
+	vxlanPrefix = "vxlan-"
+
+	maxDeviceNameLength = 15 // max length for a network device name in Linux
+
+)
+
+func CreateBridge(name string, mtu int, gateway netip.Addr) (netlink.Link, error) {
+
+	// ensure the bridge name is within the character limit
+	bridgeName, err := GenerateDeviceName(brPrefix, name)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate bridge name: %w", err)
+	}
 
 	// check if the bridge already exists
 	if l, _ := netlink.LinkByName(bridgeName); l != nil {
@@ -49,4 +66,65 @@ func CreateBridge(bridgeName string, mtu int, gateway netip.Addr) (netlink.Link,
 	}
 
 	return dev, nil
+}
+
+// using netlink to generate a device name within a certain character limit
+func GenerateDeviceName(prefix string, name string) (string, error) {
+
+	if len(prefix) > maxDeviceNameLength {
+		return "", fmt.Errorf("prefix %s exceeds max length %d", prefix, maxDeviceNameLength)
+	}
+
+	hashLen := maxDeviceNameLength - len(prefix)
+
+	hash := sha1.New()
+	hash.Write([]byte(name))
+
+	hashedName := hex.EncodeToString(hash.Sum(nil))
+
+	return prefix + hashedName[:hashLen], nil
+
+}
+
+func FirstIP(ip *net.IPNet) (*net.IPNet, error) {
+
+	// extract the network address
+	netIP := ip.IP.Mask(ip.Mask)
+	ipLen := len(netIP)
+
+	// copy it to a slice
+	first := make(net.IP, ipLen)
+	copy(first, netIP)
+
+	// create a new IPNet with the first IP and the same mask
+	// Add 1 to the last byte, with carry
+	for i := ipLen - 1; i >= 0; i-- {
+		first[i]++
+		if first[i] != 0 {
+			break
+		}
+		// else overflowed, carry to next more significant byte
+	}
+
+	if !ip.Contains(first) {
+		return nil, fmt.Errorf("first IP %s is not in the network %s", first, ip)
+	}
+
+	return &net.IPNet{
+		IP:   first,
+		Mask: ip.Mask,
+	}, nil
+
+}
+
+func HostIP(ip *net.IPNet) (*net.IPNet, error) {
+
+	// extract the network address - guaranteed to be a valid IP - ip.IP might not be the network address
+	hostIP := ip.IP.Mask(ip.Mask)
+
+	return &net.IPNet{
+		IP:   hostIP,
+		Mask: ip.Mask,
+	}, nil
+
 }
