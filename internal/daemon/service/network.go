caa4e01af9419fef592d477796ef4b5b459f7c7e
Joao Vicente <joaogvicente@gmail.com>
2025-09-07 18:35:48 +0100

    STR-15 second test

--- internal/daemon/service/network.go
+++ internal/daemon/service/network.go
@@ -10,7 +10,7 @@ import (
 	// api types
 	seterav1 "github/setera/pkg/api/setera.com/v1"
 
-	// pkg
+	// pkg - "second test"
 
 	"github/setera/pkg/network"
 	// backend - // change to test the integration between jira and github
